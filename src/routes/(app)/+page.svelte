57d7fad10356e4dabb959d86c0e63d66d29c8277
Itachi-MacOS <warrior.006@inbox.ru>
2023-08-24 05:42:16 +0600

    experimenting with queryparam as an app state

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,8 +1,27 @@
 <script lang="ts">
+	import { queryParam } from 'sveltekit-search-params';
+	import { browser } from '$app/environment';
 	import AppHeader from '$lib/ui/navbar/ui-navbar.svelte';
 	import SearchIcon from '$lib/icons/search-icon.svelte';
 	import FilterIcon from '$lib/icons/filter-icon.svelte';
 	import FeatureMosqueList from '$lib/features/mosque-list/feature-mosque-list.svelte';
+	import useCity from '$lib/hooks/useCity.js';
+	import useGender from '$lib/hooks/useGender.js';
+	queryParam('city').subscribe((value) => {
+		console.log(value, 'subscribe fn');
+	});
+	const cityQuery = queryParam('city');
+	const genderQuery = queryParam('gender');
+	const [getCity] = useCity();
+	const { getGender } = useGender();
+	if (browser) {
+		const city = getCity()?.id;
+		if (!$cityQuery) {
+			cityQuery.set(city ? `${city}` : null);
+		}
+		genderQuery.set(getGender() || 'male');
+	}
+
 	export let data;
 
 	$: ({ cities, banners, mosques } = data);
@@ -12,7 +31,13 @@
 	<svelte:fragment slot="leftIcon">
 		<SearchIcon />
 	</svelte:fragment>
-	<div>Все</div>
+	<div>
+		{#if $cityQuery}
+			{cities.find((x) => x.id === Number($cityQuery))?.name}
+		{:else}
+			Все
+		{/if}
+	</div>
 	<svelte:fragment slot="rightIcon">
 		<a href="/filter">
 			<FilterIcon />
